bc20eb66bea0e4eb1c8c90e63b4f250f16d27d6a
Aditya Thakker <Aditya.Thakker@gmail.com>
2019-01-15 13:28:06 -0500

    Added splash

---
+++ src/Mobile/EAP.Xamarin.Core/EAP.Xamarin.Core.Android/SplashActivity.cs
@@ -0,0 +1,26 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace EAP.Xamarin.Core.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true, ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashActivity : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        public override void OnBackPressed() { }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
